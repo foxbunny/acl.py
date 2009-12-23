17ef2dda8cafae4e4ced6bcdb889a53bd84737a4
Branko Vukelic <bg.branko@gmail.com>
2009-12-23 21:48:46 +0100

    Import the database object into auth module

--- authenticationpy/auth.py
+++ authenticationpy/auth.py
@@ -1,12 +1,20 @@
 import web
+import re
+from authenticationpy import DATABASE
+
+db = DATABASE
 
 TABLE = 'authenticationpy_users'
 
-class User(self):
+# Usernames must start with a letter, and can contain letters, numbers, dots,
+# dashes, and underscores
+username_re = re.compile(r'[A-Za-z]{1}[A-Za-z0-9.-_]{3,39}')
+
+class User():
     """ User and user management class
     """
 
-    def __init__(self, username, password, email):
+    def __init__(self, username, email):
         self.username = None
         self.password = None
         self.email = None
