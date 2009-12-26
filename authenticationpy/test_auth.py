10480d582d25c622686261fd5e1d8ea2bb96d48a
Branko Vukelic <bg.branko@gmail.com>
2009-12-26 02:52:21 +0100

    Test that dirty_fields is empty right after calling get_user

--- authenticationpy/test_auth.py
+++ authenticationpy/test_auth.py
@@ -244,3 +244,10 @@ def test_authenticate_wrong_password():
 def test_user_has_dirty_fields_property():
     user = auth.User(username='myuser', email='valid@email.com')
     assert user.dirty_fields == []
+
+@with_setup(setup=setup_table, teardown=teardown_table)
+def test_dirty_fields_empty_after_get_user():
+    user = auth.User(username='myuser', email='valid@email.com')
+    user.create()
+    user = auth.User.get_user(username='myuser')
+    assert user.dirty_fields == []
