f3bcadae2f4307bac8587285dd104d1d46b70be9
Branko Vukelic <bg.branko@gmail.com>
2009-12-30 11:04:04 +0100

    Changed argument URLs to include username and /arguments/ subpath

--- debate_example_app/app.py
+++ debate_example_app/app.py
@@ -7,8 +7,8 @@ urls = (
     '/new', 'new_debate',
     '/(?P<title>[\w_]+)/', 'debate',
     '/(?P<title>[\w_]+)/delete', 'delete_debate',
-    '/(?P<title>[\w_]+)/edit', 'edit_debate',
-    '/(?P<title>[\w_]+)/(?P<id>\d+)', 'argument'
+    '/(?P<title>[\w_]+)/arguments/(?P<username>.*)/', 'argument',
+    '/(?P<title>[\w_]+)/arguments/(?P<username>.*)/delete', 'delete_argument'
 )
 
 app = web.application(urls, globals())
