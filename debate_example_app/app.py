a2697bc0049c2b3139838e61f2f7c6e06351695c
Branko Vukelic <bg.branko@gmail.com>
2009-12-30 10:53:43 +0100

    Added URLs

--- debate_example_app/app.py
+++ debate_example_app/app.py
@@ -2,7 +2,14 @@ import web
 
 import config
 
-urls = ()
+urls = (
+    '/', 'debates',
+    '/new', 'new_debate',
+    '/(?P<title>[\w_]+)/', 'debate',
+    '/(?P<title>[\w_]+)/delete', 'delete_debate',
+    '/(?P<title>[\w_]+)/edit', 'edit_debate',
+    '/(?P<title>[\w_]+)/(?P<id>\d+)', 'argument'
+)
 
 app = web.application(urls, globals())
 
