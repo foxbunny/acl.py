3c77bea7930c9cb51973991bee3a082b186f9cf0
Branko Vukelic <bg.branko@gmail.com>
2009-12-30 11:15:41 +0100

    Added URLs for login, log out, account removal, and registration

--- debate_example_app/app.py
+++ debate_example_app/app.py
@@ -9,6 +9,10 @@ urls = (
     '/(?P<title>[\w_]+)/delete', 'delete_debate',
     '/(?P<title>[\w_]+)/arguments/(?P<username>.*)/', 'argument',
     '/(?P<title>[\w_]+)/arguments/(?P<username>.*)/delete', 'delete_argument'
+    '/login', 'login',
+    '/logoff', 'logoff',
+    '/register', 'register',
+    '/unregister', 'unregister',
 )
 
 app = web.application(urls, globals())
