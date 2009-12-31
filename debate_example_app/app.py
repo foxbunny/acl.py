d91fc0d77ace2f928c0cbb1b42888ed6d97f80f9
Branko Vukelic <bg.branko@gmail.com>
2009-12-31 19:23:55 +0100

    Render login form on login pate

--- debate_example_app/app.py
+++ debate_example_app/app.py
@@ -2,6 +2,7 @@ import web
 
 import config
 from debate import *
+from auth_forms import login_form, register_form
 
 urls = (
     '/', 'debates',
@@ -16,6 +17,13 @@ urls = (
     '/unregister', 'unregister',
 )
 
+render = web.template.render('templates')
+
+class login:
+    def GET(self):
+        f = login_form()
+        content = render.login_page(f)
+        return render.base_clean(content)
 app = web.application(urls, globals())
 
 web.config.session = web.session.Session(app, config.sess_store, config.sess_init)
