61e49e71728a8f22059389da68191152d79e229c
Branko Vukelic <bg.branko@gmail.com>
2009-12-31 01:56:21 +0100

    Implement debates listing

--- debate_example_app/debate.py
+++ debate_example_app/debate.py
@@ -46,8 +46,15 @@ login_form_html = render_login_form()
 
 class debates:
     def GET(self):
-        pass
-
+        # we have stored username in session at some point if authenticated
+        uname = web.config.session.get('user')
+        # we will take ``user == None`` to mean 'not authenticated'
+        user = None
+        if uname:
+            # if username is not ``None``, grab the user from the database
+            user = User.get_user(username=uname)
+        debates = web.config.db.select('debates')
+        return render.debates(debates, user, login_form_html)
 
 class new_debate:
     def POST(self):
