139038ebc10094a9c83fbcec144d774ef8590c14
Branko Vukelic <bg.branko@gmail.com>
2009-12-31 01:52:46 +0100

    Use web.config.db instead of config.db

--- debate_example_app/config.py
+++ debate_example_app/config.py
@@ -1,14 +1,14 @@
 import web
 
 # modify the parameters according to your own situation
-db = web.database(dbn='postgres', db='debate_example', user='postgres')
+web.config.db = web.database(dbn='postgres', db='debate_example', user='postgres')
 
 # site configuration
 sitename = 'Debate club'
 web.config.admin_user = 'administrator'
 
 # authentication.py config
-web.config.authdb = db
+web.config.authdb = web.config.db
 web.config.authmail = {'sender': 'your.email@server.com',
                        'activation_subject': '%s: Account activation' % sitename,
                        'reset_subject': '%s: Password reset' % sitename,
