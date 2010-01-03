e65cd5e01b924aa974239569e54383b65260d485
Branko Vukelic <bg.branko@gmail.com>
2010-01-03 15:23:58 +0100

    Unified change password and reset password under same URL, added done URL

--- debate_example_app/app.py
+++ debate_example_app/app.py
@@ -16,8 +16,7 @@ urls = (
     '/logoff', 'logoff',
     '/register(/done|)', 'register',
     '/unregister', 'unregister',
-    '/reset_password', 'reset_password',
-    '/change_password', 'change_password',
+    '/reset_password(/done|)', 'reset_password',
     '/confirm/(a|d|r)/([a-f0-9]{64})', 'confirm',
     '/confirm/request_code/(a|d|r|done)', 'request_code',
 )
